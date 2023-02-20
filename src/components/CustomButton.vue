e205cde80bfbaf59c5205874dc3f36e1b81e60b5
Nalam Manasa Venkata Naga Sai <93673311+NalamManasa@users.noreply.github.com>
2023-02-20 15:11:56 +0530

    Add files via upload

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="onClickButton">CLICK HERE</button>
+</template>
+<style scoped>
+button{
+  padding: 4px 10px;
+  border-radius: 6px;
+  background-color: lightgray;
+}
+button:hover{
+  background-color: white;
+ 
+}
+</style>
+<script setup>
+let count = 0;
+const emits = defineEmits(["button-clicked"]);
+
+const onClickButton = function(){
+  count += 1;
+  emits("button-clicked",count)
+}
+</script>
